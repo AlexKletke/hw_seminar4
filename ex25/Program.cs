dc73fa5f2d2000fbd2880352796bb5166704851a
AlexandrKletke <klete@mail.ru>
2022-11-29 23:32:29 +0300

    wrote solution the 25th example

---
+++ ex25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+using static System.Console;
+double CycleDegree(int A, int B)
+{
+    double Result = 0;
+    Result = Math.Pow(A, B);
+    return Result;
+}
+Write("enter A: ");
+int A = int.Parse(ReadLine());
+Write("enter B > 0: ");
+int B = int.Parse(ReadLine());
+double Result = CycleDegree(A, B);
+CycleDegree(A, B);
+WriteLine(Result);
+
